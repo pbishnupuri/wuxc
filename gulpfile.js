ca326642e58f068c006ca3377893b44ee36d2ae4
Pratyay Bishnupuri <pbishnupuri@wustl.edu>
2019-06-16 23:18:53 -0500

    added bootstrap styling, gulpwatch, browser sync, and popper.js

---
+++ gulpfile.js
@@ -0,0 +1,25 @@
+const gulp = require('gulp');
+const sass = require('gulp-sass');
+const browserSync = require('browser-sync').create();
+
+//comple scss into css
+function style(){
+    return gulp.src('./src/scss/**/*.scss')
+    .pipe(sass().on('error', sass.logError))
+    .pipe(gulp.dest('./src/css'))
+    .pipe(browserSync.stream());
+}
+
+function watch(){
+    browserSync.init({
+        server:{
+            baseDir: "./src"
+        }
+    });
+    gulp.watch('src/scss/**/*.scss', style);
+    gulp.watch('src/*.html').on('change', browserSync.reload);
+    gulp.watch('src/js/**/*.js').on('change', browserSync.reload);
+}
+
+exports.style = style;
+exports.watch = watch;
